f1861ce7f1d1e2ac9c11abe6a50a5f29ec0d4356
Autmn134F <36757974+CeraCharlesCC@users.noreply.github.com>
2025-12-31 18:46:15 +0900

    Update Administration.kt

--- src/main/kotlin/io/github/ceracharlescc/lmversusu/internal/infrastructure/ktor/Administration.kt
+++ src/main/kotlin/io/github/ceracharlescc/lmversusu/internal/infrastructure/ktor/Administration.kt
@@ -1,9 +1,12 @@
 package io.github.ceracharlescc.lmversusu.internal.infrastructure.ktor
 
+import io.github.flaxoos.ktor.server.plugins.ratelimiter.CallVolumeUnit
 import io.github.flaxoos.ktor.server.plugins.ratelimiter.RateLimiting
 import io.github.flaxoos.ktor.server.plugins.ratelimiter.implementations.TokenBucket
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
+import io.ktor.server.response.respond
 import kotlin.time.Duration.Companion.seconds
 
 internal fun Application.configureAdministration() {
@@ -11,8 +14,16 @@ internal fun Application.configureAdministration() {
 
         rateLimiter {
             type = TokenBucket::class
-            capacity = 100
+            capacity = 200
             rate = 10.seconds
+            callVolumeUnit = CallVolumeUnit.Calls()
+        }
+
+        rateLimitExceededHandler = { limitedBy ->
+            respond(HttpStatusCode.TooManyRequests, "Rate limit exceeded: ${limitedBy.message}")
+            response.headers.append("X-RateLimit-Limit", "${limitedBy.rateLimiter.capacity}")
+            response.headers.append("X-RateLimit-Measured-by", limitedBy.rateLimiter.callVolumeUnit.name)
+            response.headers.append("X-RateLimit-Reset", "${limitedBy.resetIn.inWholeMilliseconds}")
         }
     }
 }
